a331e6eaa2d6980bb434ae6c8f0fc8d14534b37f
Róbert Kiss <ert78gb@gmail.com>
2016-09-24 01:00:48 +0200

    export errorHandler

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Server = require('./src/server'),
-    ValidationError = require('./src/errors/validationError')
+    ValidationError = require('./src/errors/validationError'),
+    errorHandler = require('./src/errors/errorHandler')
 ;
 
 exports = module.exports = Server;
 
 exports.ValidationError = ValidationError;
+exports.errorHandler = errorHandler;
